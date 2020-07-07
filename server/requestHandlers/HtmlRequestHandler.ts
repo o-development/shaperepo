c7647fa1cf72b0140f3a2f158d5374a8afd9b4bd
Jackson Morgan <jackson@MacBook-Pro.local>
2020-07-07 14:59:11 -0400

    HTML request handler finished

--- server/requestHandlers/HtmlRequestHandler.ts
+++ server/requestHandlers/HtmlRequestHandler.ts
@@ -1,13 +1,28 @@
 import IRequestHandler from "./IRequestHandler";
 import { Request, Response } from "express";
+import { parse, UrlWithParsedQuery } from 'url'
+import next from 'next'
+import Server from "next/dist/next-server/server/next-server";
+import { IncomingMessage, ServerResponse } from "http";
+
+const dev = process.env.NODE_ENV !== "production";
 
 class HtmlRequestHandler implements IRequestHandler {
-  async init(): Promise<void> {
+  private app: Server
+  private handler: (req: IncomingMessage, res: ServerResponse, parsedUrl?: UrlWithParsedQuery | undefined) => Promise<void>
 
+  constructor() {
+    this.app = next({ dev })
+    this.handler = this.app.getRequestHandler()
   }
 
-  async handle(_req: Request, res: Response): Promise<void> {
-    res.send("HTML")
+  async init(): Promise<void> {
+    await this.app.prepare()
+  }
+
+  async handle(req: Request, res: Response): Promise<void> {
+    const parsedUrl = parse(req.url!, true)
+    await this.handler(req, res, parsedUrl)
   }
 }
 
